2ee1e8599a04c251c6d5d96a2ab683343dd3422e
eoqthiago <contatopixthiago@gmail.com>
2022-04-11 22:52:25 -0300

    arrumando

---
+++ src/pages/home/app.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+// function imgcarro () {
+//     let image = document.getElementById("image");
+//     if(image.src === "../../assets/images/carro-quaselado.png"){
+//         image.src = "../../assets/images/bmw-m6-lado.png";
+//     }
+//     else if(image.src === "../../assets/images/bmw-m6-lado.png"){
+//         image.src = "../../assets/images/carro-quaselado.png";
+//     }
+// }
+// imgcarro();
+
+// function imgcarro(){
+//     if(document.getElementById("image").src = "../../assets/images/bmw-m6-lado.png"){
+//         document.getElementById("image").src = "../../assets/images/carro-quaselado.png";
+//     } else if(document.getElementById("image").src = "../../assets/images/carro-quaselado.png"){
+//         document.getElementById("image").src = "../../assets/images/bmw-m6-lado.png"
+//     }
+
+// }
+
+// imgcarro();
