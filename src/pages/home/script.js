bdf1f68eb9000721ffbec9b86157ab9b8b02631a
Thierry <thierryalefhsanto@gmail.com>
2022-04-13 22:51:45 -0300

    algumas alterações na apresentação

--- src/pages/home/script.js
+++ src/pages/home/script.js
@@ -22,19 +22,21 @@ imgcarro();*/
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 260 || document.documentElement.scrollTop > 260) {
+  if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
     document.getElementById("header").style.height = "50px";
+    document.getElementById("header").style.backgroundColor = "#55595c";
     document.getElementById("header-logo").style.fontSize = "2em";
     document.getElementById("header-item").style.fontSize = "0.98em";
     document.getElementById("user").style.transform = "scale(0.8)";
   } else {
     document.getElementById("header").style.height = "";
+    document.getElementById("header").style.backgroundColor = "";
     document.getElementById("header-logo").style.fontSize = "";
     document.getElementById("header-item").style.fontSize = "1em";
     document.getElementById("user").style.transform = "";
   }
 
-  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+  if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
     document.getElementById("return").style.display = "initial";
   } else {
     document.getElementById("return").style.display = "none";
