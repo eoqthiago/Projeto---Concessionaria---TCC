fadddb41661080ec61a9ad97adc46e6ea64dd5b1
othierrydaora <thierryalefhsanto@gmail.com>
2022-05-08 22:28:27 -0300

    Alterações gerais e implementação de cabeçalho simples

--- src/common/script.js
+++ src/common/script.js
@@ -6,7 +6,7 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
     document.getElementById("header").style.height = "50px";
-    document.getElementById("header").style.backgroundColor = "#55595c";
+    document.getElementById("header").style.backgroundColor = "#000";
     document.getElementById("header-logo").style.fontSize = "2em";
     document.getElementById("header-item").style.fontSize = "0.98em";
     document.getElementById("user").style.transform = "scale(0.8)";
